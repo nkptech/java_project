84cbb326ac98b7c24225a75b3780b9672bfd78d6
Prashant <nkptech@gmail.com>
2023-01-19 12:54:20 +0530

    java files committed

---
+++ Addition/src/addnumbers.java
@@ -0,0 +1,13 @@
+
+public class addnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		
+		System.out.println("sum of a and b is :"+ c);
+	}
+
+}
