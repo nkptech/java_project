84a8b5a36a977172e682bd256b2943ce8dc29c22
nkptech <63147022+nkptech@users.noreply.github.com>
2023-01-19 13:21:48 +0530

    number changed

--- Addition/src/addnumbers.java
+++ Addition/src/addnumbers.java
@@ -4,7 +4,7 @@ public class addnumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 1;
-		int b = 2;
+		int b = 5;
 		int c = a+b;
 		
 		System.out.println("sum of a and b is :"+ c);
