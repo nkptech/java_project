9122db9f609adda7599d1736964149a158bf6f48
nkptech <63147022+nkptech@users.noreply.github.com>
2023-01-19 15:14:13 +0530

    sencond commit a and b

--- Addition/src/addnumbers.java
+++ Addition/src/addnumbers.java
@@ -3,8 +3,8 @@ public class addnumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
-		int b = 5;
+		int a = 10;
+		int b = 25;
 		int c = a+b;
 		
 		System.out.println("sum of a and b is :"+ c);
